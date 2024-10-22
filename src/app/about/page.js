f1caea595fb5ea4634765a3cd809d51feecd3ebf
Matthew Fourie <mattfourie3@gmail.com>
2024-10-22 13:21:05 +0200

    Added finishing touches to the about page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,6 +15,7 @@ export default function About() {
      setExpanded(isExpanded ? panel : false);
    };
 
+  //  Content for the hero section
   const sections = [
     { 
       title: 'OUR VISION', 
@@ -32,6 +33,7 @@ export default function About() {
     },
   ];
 
+  // Content for the dropdown menu
   const activitiesItems = [
     {
       question: "PROVIDE NUTRITIONAL SUPPORT",
