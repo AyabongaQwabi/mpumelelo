3e19a5afd3a96be3052955dcd55a164cb1b191ec
zwelisha <zwescientist@gmail.com>
2024-10-23 05:54:42 +0200

    Removed unused code in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
 'use client';
-import Head from 'next/head';
-import Header from './components/Header';
 import Hero from './components/Hero';
-import ContentSection from './components/ContentSection';
 import Footer from './components/Footer';
 import CustomCarousel from './components/HomeCarouselItems';
-import { Box, Button, Typography, Container, Grid, Card, CardContent, CardMedia, Accordion, AccordionSummary, AccordionDetails, Divider } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import IntroductionSection from './components/IntroductionSecton';
 
 
@@ -14,18 +9,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      {/* <ContentSection
-        title='Our Work'
-        description='Learn more about our efforts to make the world a better place.'
-        items={[
-          {
-            title: 'Education',
-            content: 'Empowering youth through education.',
-          },
-          { title: 'Health', content: 'Improving global health for everyone.' },
-          { title: 'Environment', content: 'Promoting sustainable practices.' },
-        ]}
-      /> */}
       {/* Grid of content and Images */}
       <IntroductionSection />
       <CustomCarousel />
